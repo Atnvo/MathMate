0e03393addb31dee7faee0f9c2f6b757b72286c8
Atnvo <30607368+Atnvo@users.noreply.github.com>
2018-06-01 10:10:42 +0200

    v1.41

--- Login/logindb.php
+++ Login/logindb.php
@@ -128,13 +128,13 @@
 				<tr>
 					<td>Username:</td>
 					<td>
-						<input type="text" name="username" />
+						<input type="text" name="username" required />
 					</td>
 				</tr>
 				<tr>
 					<td>Passsword:</td>
 					<td>
-						<input type="password" name="password" />
+						<input type="password" name="password" required />
 					</td>
 				</tr>
 			</table>
@@ -159,14 +159,14 @@
 							<tr>
 								<td>username: </td>
 								<td>
-									<input type="text" name="username" />
+									<input type="text" name="username" required />
 									<br>
 								</td>
 							</tr>
 							<tr>
 								<td>password: </td>
 								<td>
-									<input type="password" name="password" />
+									<input type="password" name="password" required />
 									<br>
 								</td>
 							</tr>
